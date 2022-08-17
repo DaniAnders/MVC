13345edba3e185205597e5517763813e3af635d6
DaniAnders <serafim.andersson.danielle@gmail.com>
2022-08-17 13:40:02 +0200

    basic1

---
+++ MVC/Program.cs
@@ -0,0 +1,42 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+/* services mvc */
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapControllerRoute(
+    name: "index",
+    pattern: "index",
+    defaults: new {controller = "Home", action = "Index" });
+
+
+app.MapControllerRoute(
+    name: "about",
+    pattern: "about",
+    defaults: new { controller = "Home", action = "About" });
+
+
+app.MapControllerRoute(
+    name: "projects",
+    pattern: "projects",
+    defaults: new { controller = "Home", action = "Projects" });
+
+
+app.MapControllerRoute(
+    name: "contact",
+    pattern: "contact",
+    defaults: new { controller = "Home", action = "contact" });
+
+
+app.Run();
+
